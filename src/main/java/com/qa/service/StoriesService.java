6ec479d5fe6d314c397d2056ddfca7d91c8a10b5
MDunnageQA <matthew.dunnage.qa@gmail.com>
2020-05-07 09:59:52 +0100

    created Stories service

---
+++ src/main/java/com/qa/service/StoriesService.java
@@ -0,0 +1,21 @@
+package com.qa.service;
+
+import com.qa.repository.StoriesRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StoriesService {
+
+    private final StoriesRepository repo;
+
+    //private final ModelMapper mapper;
+
+    @Autowired
+    public StoriesService(StoriesRepository repo/*, ModelMapper mapper*/) {
+        this.repo = repo;
+        //this.mapper = mapper;
+    }
+
+}
