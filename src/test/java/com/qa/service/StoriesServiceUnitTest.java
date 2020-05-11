1598113f9882580f460a120bdb998f38843130ed
MDunnageQA <matthew.dunnage.qa@gmail.com>
2020-05-11 08:44:40 +0100

    added unit test to create stories

--- src/test/java/com/qa/service/StoriesServiceUnitTest.java
+++ src/test/java/com/qa/service/StoriesServiceUnitTest.java
@@ -1,4 +1,65 @@
 package com.qa.service;
 
+import com.qa.domain.Stories;
+import com.qa.dto.StoriesDTO;
+import com.qa.exceptions.StoriesNotFoundException;
+import com.qa.repository.StoriesRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.modelmapper.ModelMapper;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
 public class StoriesServiceUnitTest {
+
+    @InjectMocks
+    private StoriesService service;
+
+    @Mock
+    private StoriesRepository repository;
+
+    @Mock
+    private ModelMapper mapper;
+
+    private List<Stories> storiesList;
+
+    private Stories testStories;
+
+    private Long id = 1L;
+
+    private Stories testStoriesWithID;
+
+    private StoriesDTO storiesDTO;
+
+    private StoriesDTO maptoDTO(Stories stories) {
+        return this.mapper.map(stories, StoriesDTO.class);
+    }
+
+    @Before
+    public void testsSetUp() {
+        this.storiesList = new ArrayList<>();
+        this.testStories = new Stories("title", "genre", "content");
+        this.storiesList.add(testStories);
+        this.testStoriesWithID = new Stories(testStories.getTitle(), testStories.getGenre(), testStories.getContent());
+        this.testStoriesWithID.setId(id);
+        this.storiesDTO = this.maptoDTO(testStoriesWithID);
+    }
+
+    @Test
+    public void getAllStoriesTest() {
+        when(repository.findAll()).thenReturn(this.storiesList);
+        when(this.mapper.map(testStoriesWithID, StoriesDTO.class)).thenReturn(storiesDTO);
+        assertFalse("Service returned no Stories", this.service.readStories().isEmpty());
+        verify(repository, times(1)).findAll();
+    }
 }
