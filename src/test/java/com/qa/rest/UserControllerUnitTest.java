805d5a1d8a65fd8a2397862e6b9430ea70dce9f2
MDunnageQA <matthew.dunnage.qa@gmail.com>
2020-05-13 11:23:04 +0100

    added unit test for reading and creating in the user controller

--- src/test/java/com/qa/rest/UserControllerUnitTest.java
+++ src/test/java/com/qa/rest/UserControllerUnitTest.java
@@ -1,4 +1,76 @@
 package com.qa.rest;
 
+import com.qa.domain.User;
+import com.qa.dto.UserDTO;
+import com.qa.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
 public class UserControllerUnitTest {
+
+    @InjectMocks
+    private UserController userController;
+
+    @Mock
+    private UserService service;
+
+    private List<User> userList;
+
+    private User testUser;
+
+    private User testUserWithId;
+
+    private long id = 1L;
+
+    private UserDTO userDTO;
+
+    private final ModelMapper mapper = new ModelMapper();
+
+    private UserDTO mapToDTO(User user){
+        return this.mapper.map(user, UserDTO.class);
+    }
+
+    @Before
+    public void setUpForTests() {
+        this.userList = new ArrayList<>();
+        this.testUser = new User("username", "password",
+                "firstname", "surname", "DOB", "email");
+        this.userList.add(testUser);
+        this.testUserWithId = new User(testUser.getUserName(), testUser.getPassword(),
+                testUser.getFirstName(), testUser.getSurname(), testUser.getDateOfBirth(), testUser.getEmail());
+        this.testUserWithId.setId(this.id);
+        this.userDTO = this.mapToDTO(testUserWithId);
+    }
+
+    @Test
+    public void getAllUsersTest() {
+        when(service.readUsers()).thenReturn(this.userList.stream().map(this::mapToDTO).collect(
+                Collectors.toList()));
+        assertFalse("No Users found", this.userController.getAllUsers().getBody().isEmpty());
+        verify(service, times(1)).readUsers();
+    }
+
+    @Test
+    public void createUserTest() {
+        when(this.service.createUser(testUser)).thenReturn(this.userDTO);
+        assertEquals(this.userController.createUser(testUser), new ResponseEntity<UserDTO>(
+                this.userDTO, HttpStatus.CREATED));
+        verify(this.service, times(1)).createUser(testUser);
+    }
 }
